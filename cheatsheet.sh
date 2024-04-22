17d7bd8605d1ce1b0225dccf2fa8281d450be94e
Yair Finesilver <yair.yfs@gmail.com>
2024-04-22 10:49:38 +0300

    bump version and date

--- cheatsheet.sh
+++ cheatsheet.sh
@@ -54,8 +54,8 @@ errorNoDir="error - directory not exists, please provide a valid directory"
 errorNoMode="error - no mode set"
 errorAdd="error - this command exists already in the cheatsheet"
 errorRemove="error - this line is not available - cheatsheet has only "
-version1="version:                 2.1.0"
-version2="date of last change:     20170906"
+version1="version:                 2.1.1"
+version2="date of last change:     20240422"
 version3="author:                  Michael Wellner (@m1well)"
 
 ### file ###
